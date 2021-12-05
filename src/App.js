24988676aa47c33c316d3ea296029f0f1ac9df88
Sidd Bhattacherya <sidd.bhattacherya3@gmail.com>
2021-12-05 16:15:33 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TonalComponent extends React.Component{
+  
+  render(){
+      return (
+    
+        <div>
+         
+        <div className="bubble">
+          <div className="label">{this.props.label}</div>
+          <div className="weight">{this.props.weight}</div>
+          <div className="unit">lbs</div>
+        </div>
+
+        </div>
+        
+    );
+
+  }
+ 
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: 'Label',
+      weight: 'Weight',
+      unit: 'Unit'
+  
+  };
+
+    this.handleChangeLabel = this.handleChangeLabel.bind(this);
+    this.handleChangeWeight = this.handleChangeWeight.bind(this);
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeLabel(event) {
+    this.setState({label: event.target.value});
+  }
+  handleChangeWeight(event) {
+    this.setState({weight: event.target.value});
+  }
+ 
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.label);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+        <TonalComponent label={this.state.label} weight = {this.state.weight}/>
+        <div className="form">
+          <form onSubmit={this.handleSubmit}>
+            <label className="formLabel">
+            
+              Label
+            <input type="text" value={this.state.label} onChange={this.handleChangeLabel} />
+            </label>
+            <label className="formWeight">
+            
+              Weight
+            <input type="text" value={this.state.weight} onChange={this.handleChangeWeight} />
+            </label>
+                        
+            
+          </form>
+          
+           </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
